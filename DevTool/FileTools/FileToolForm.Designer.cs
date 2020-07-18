bf0689b34652c57f30be864fd9b0e364a02f1b80
joeyhuang <joeyhuang312@gmail.com>
2020-07-18 10:42:43 +0800

    文件工具

--- DevTool/FileTools/FileToolForm.Designer.cs
+++ DevTool/FileTools/FileToolForm.Designer.cs
@@ -28,20 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputFileName = new System.Windows.Forms.TextBox();
+            this.selectFilesBtn = new System.Windows.Forms.Button();
+            this.changeNameBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "批量修改文件名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "文件名首部分";
+            // 
+            // InputFileName
+            // 
+            this.InputFileName.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.InputFileName.Location = new System.Drawing.Point(124, 59);
+            this.InputFileName.Name = "InputFileName";
+            this.InputFileName.Size = new System.Drawing.Size(171, 33);
+            this.InputFileName.TabIndex = 3;
+            // 
+            // selectFilesBtn
+            // 
+            this.selectFilesBtn.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.selectFilesBtn.Location = new System.Drawing.Point(327, 57);
+            this.selectFilesBtn.Name = "selectFilesBtn";
+            this.selectFilesBtn.Size = new System.Drawing.Size(120, 35);
+            this.selectFilesBtn.TabIndex = 5;
+            this.selectFilesBtn.Text = "选择文件";
+            this.selectFilesBtn.UseVisualStyleBackColor = true;
+            this.selectFilesBtn.Click += new System.EventHandler(this.selectFilesBtn_Click);
+            // 
+            // changeNameBtn
+            // 
+            this.changeNameBtn.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.changeNameBtn.Location = new System.Drawing.Point(472, 57);
+            this.changeNameBtn.Name = "changeNameBtn";
+            this.changeNameBtn.Size = new System.Drawing.Size(120, 35);
+            this.changeNameBtn.TabIndex = 6;
+            this.changeNameBtn.Text = "修改";
+            this.changeNameBtn.UseVisualStyleBackColor = true;
+            this.changeNameBtn.Click += new System.EventHandler(this.changeNameBtn_Click);
             // 
             // FileToolForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(944, 601);
+            this.Controls.Add(this.changeNameBtn);
+            this.Controls.Add(this.selectFilesBtn);
+            this.Controls.Add(this.InputFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FileToolForm";
             this.Text = "FileToolForm";
             this.Load += new System.EventHandler(this.FileToolForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputFileName;
+        private System.Windows.Forms.Button selectFilesBtn;
+        private System.Windows.Forms.Button changeNameBtn;
     }
 }
--- DevTool/FileTools/FileToolForm.cs
+++ DevTool/FileTools/FileToolForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Code;
 
 namespace DevTool.FileTools
 {
@@ -20,6 +22,31 @@ namespace DevTool.FileTools
         private void FileToolForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        List<string> fileNameLists = new List<string>();
+        private void selectFilesBtn_Click(object sender, EventArgs e)
+        {
+            fileNameLists.Clear();
+            FileSelectTool.SelectFiles(out fileNameLists);
+            foreach (var _name in fileNameLists)
+            {
+                Shell.WriteLine(Path.GetFileName(_name));
+            }
+        }
+
+        private void changeNameBtn_Click(object sender, EventArgs e)
+        {
+            if (fileNameLists.Count == 0) return;
+            int index = 1;
+            string path = Path.GetDirectoryName(fileNameLists[0]);
+            foreach (string fileName in fileNameLists)
+            {
+                string extendsionStr = Path.GetExtension(fileName);
+                File.Move(fileName, path + "\\" + InputFileName.Text + index + extendsionStr);
+                index++;
+            }
+            Shell.WriteLine("完成");
         }
     }
 }
